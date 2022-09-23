798ff7fc825387f1d87c114dd74827a8f079fe68
Igor Bugaiov <no-reply>
2022-09-23 10:52:56 +0300

    st0-mod4.1-t5 1st try

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,14 @@
 package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
+    // Create a program to check whether a year is a leap year or not
+    // ("leap" if true otherwise "not leap").
+    // Year will be passed as argument in method parameter:
     public void isLeapYear(int year) {
+        if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+            System.out.println("leap");
+        } else {
+            System.out.println("not leap");
+        }
     }
 }
